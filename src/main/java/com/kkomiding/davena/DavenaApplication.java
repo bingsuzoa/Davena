63f256b59119f0e65bbe872eb36e2a72101753dc
mikyeong <zxc_777@naver.com>
2024-10-02 20:03:15 +0900

    [권미경] #1 spring 프로젝트 만들기

---
+++ src/main/java/com/kkomiding/davena/DavenaApplication.java
@@ -0,0 +1,13 @@
+package com.kkomiding.davena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kkomiding/davena/DavenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkomiding.davena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
