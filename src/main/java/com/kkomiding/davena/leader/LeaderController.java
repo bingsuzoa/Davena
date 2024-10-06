12edbba84c78a701391065bebda530d5bb4236fb
mikyeong <zxc_777@naver.com>
2024-10-06 15:15:11 +0900

    [권미경] #2 팀장 / 방만들기완료 문제 발생 시 : 이렇게 했을때는 되나? 부터 시작, 어디까지 되고, 어디부터 안되는지 파악하는게 중요 modal 사용할때 계속 오류가 발생하길래, 처음 데모 코드부터 시작해서 지워나갔더니 오류 해결함

---
+++ src/main/java/com/kkomiding/davena/leader/LeaderController.java
@@ -0,0 +1,23 @@
+package com.kkomiding.davena.leader;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/leader")
+public class LeaderController {
+	
+	@GetMapping("/login-view")
+	public String login() {
+		
+		return "leader/login";
+	}
+	
+	@GetMapping("/make-view")
+	public String makeRoom() {
+		
+		return "leader/make";
+	}
+
+}
