6cdbc572ff9f36fbc71da3b66d632e296504be8c
bingsuzoa <zxc_777@naver.com>
2024-10-27 12:36:41 +0900

    [권미경] #13 - 휴가 신청하면 work table에 저장하기

--- src/main/java/com/kkomiding/davena/work/dto/WorkDto.java
+++ src/main/java/com/kkomiding/davena/work/dto/WorkDto.java
@@ -1,28 +1,248 @@
 package com.kkomiding.davena.work.dto;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Builder(toBuilder = true)
-@NoArgsConstructor
-@AllArgsConstructor
-@Setter
-@Getter
+
 public class WorkDto {
 	
 	
-	private int id;
-	
-
 	private int userId;
-	
 	
 	private int holidayId;
 	
-	private String day1;
-
+	private String day1;	
+	private String day2;
+	private String day3;
+	private String day4;
+	private String day5;
+	private String day6;
+	private String day7;
+	private String day8;
+	private String day9;
+	private String day10;
+	private String day11;
+	private String day12;
+	private String day13;
+	private String day14;
+	private String day15;
+	private String day16;
+	private String day17;
+	private String day18;
+	private String day19;
+	private String day20;
+	private String day21;
+	private String day22;
+	private String day23;
+	private String day24;
+	private String day25;
+	private String day26;
+	private String day27;
+	private String day28;
+	private String day29;
+	private String day30;
+	private String day31;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	public int getHolidayId() {
+		return holidayId;
+	}
+	public void setHolidayId(int holidayId) {
+		this.holidayId = holidayId;
+	}
+	
+	public void setDay(int start, int end, String holiday) {
+		if(start <= 1 && 1 <= end) {
+			this.day1 = holiday;
+		}
+		if(start <= 2 && 2 <= end) {
+			this.day2 = holiday;
+		}
+		if(start <= 3 && 3 <= end) {
+			this.day3 = holiday;
+		}
+		if(start <= 4 && 4 <= end) {
+			this.day4 = holiday;
+		}
+		if(start <= 5 && 5 <= end) {
+			this.day5 = holiday;
+		}
+		if(start <= 6 && 6 <= end) {
+			this.day6 = holiday;
+		}
+		if(start <= 7 && 7 <= end) {
+			this.day7 = holiday;
+		}
+		if(start <= 8 && 8 <= end) {
+			this.day8 = holiday;
+		}
+		if(start <= 9 && 9 <= end) {
+			this.day9 = holiday;
+		}
+		if(start <= 10 && 10 <= end) {
+			this.day10 = holiday;
+		}
+		if(start <= 11 && 11 <= end) {
+			this.day11 = holiday;
+		}
+		if(start <= 12 && 12 <= end) {
+			this.day12 = holiday;
+		}
+		if(start <= 13 && 13 <= end) {
+			this.day13 = holiday;
+		}
+		if(start <= 14 && 14 <= end) {
+			this.day14 = holiday;
+		}
+		if(start <= 15 && 15 <= end) {
+			this.day15 = holiday;
+		}
+		if(start <= 16 && 16 <= end) {
+			this.day16 = holiday;
+		}
+		if(start <= 17 && 17 <= end) {
+			this.day17 = holiday;
+		}
+		if(start <= 18 && 18 <= end) {
+			this.day18 = holiday;
+		}
+		if(start <= 19 && 19 <= end) {
+			this.day19 = holiday;
+		}
+		if(start <= 20 && 20 <= end) {
+			this.day20 = holiday;
+		}
+		if(start <= 21 && 21 <= end) {
+			this.day21 = holiday;
+		}
+		if(start <= 22 && 22 <= end) {
+			this.day22 = holiday;
+		}
+		if(start <= 23 && 23 <= end) {
+			this.day23 = holiday;
+		}
+		if(start <= 24 && 24 <= end) {
+			this.day24 = holiday;
+		}
+		if(start <= 25 && 25 <= end) {
+			this.day25 = holiday;
+		}
+		if(start <= 26 && 26 <= end) {
+			this.day26 = holiday;
+		}
+		if(start <= 27 && 27 <= end) {
+			this.day27 = holiday;
+		}
+		if(start <= 28 && 28 <= end) {
+			this.day28 = holiday;
+		}
+		if(start <= 29 && 29 <= end) {
+			this.day29 = holiday;
+		}
+		if(start <= 30 && 30 <= end) {
+			this.day30 = holiday;
+		}
+		if(start <= 31 && 31 <= end) {
+			this.day31 = holiday;
+		}
+	}
+	
+	public String getDay1() {
+		return day1;
+	}
+	public String getDay2() {
+		return day2;
+	}
+	public String getDay3() {
+		return day3;
+	}
+	public String getDay4() {
+		return day4;
+	}
+	public String getDay5() {
+		return day5;
+	}
+	public String getDay6() {
+		return day6;
+	}
+	public String getDay7() {
+		return day7;
+	}
+	public String getDay8() {
+		return day8;
+	}
+	public String getDay9() {
+		return day9;
+	}
+	public String getDay10() {
+		return day10;
+	}
+	public String getDay11() {
+		return day11;
+	}
+	public String getDay12() {
+		return day12;
+	}
+	public String getDay13() {
+		return day13;
+	}
+	public String getDay14() {
+		return day14;
+	}
+	public String getDay15() {
+		return day15;
+	}
+	public String getDay16() {
+		return day16;
+	}
+	public String getDay17() {
+		return day17;
+	}
+	public String getDay18() {
+		return day18;
+	}
+	public String getDay19() {
+		return day19;
+	}
+	public String getDay20() {
+		return day20;
+	}
+	public String getDay21() {
+		return day21;
+	}
+	public String getDay22() {
+		return day22;
+	}
+	public String getDay23() {
+		return day23;
+	}
+	public String getDay24() {
+		return day24;
+	}
+	public String getDay25() {
+		return day25;
+	}
+	public String getDay26() {
+		return day26;
+	}
+	public String getDay27() {
+		return day27;
+	}
+	public String getDay28() {
+		return day28;
+	}
+	public String getDay29() {
+		return day29;
+	}
+	public String getDay30() {
+		return day30;
+	}
+	public String getDay31() {
+		return day31;
+	}
 }
